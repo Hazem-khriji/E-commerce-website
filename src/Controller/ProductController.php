50bf8270a838d4d6926a930cd8958a88d4a67952
Hazem-khriji <hazemkhriji0@gmail.com>
2025-05-09 19:37:23 +0100

    Replaced hardcoded product templates with dynamic data from DB and added full CRUD functionality via forms

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,15 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Product;
+use App\Repository\ProductRepository;
 
 final class ProductController extends AbstractController
 {
     #[Route('/product/{id}', name: 'app_product')]
-    public function index($id): Response
+    public function index(Product $product): Response
     {
         return $this->render('product/product.html.twig', [
             'controller_name' => 'ProductController',
-            'id' => $id
+            'product' => $product,
         ]);
     }
 }
--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'app_shop')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findAll();
+
         return $this->render('shop/shop.html.twig', [
             'controller_name' => 'ShopController',
+            'products' => $products,
         ]);
     }
 }
