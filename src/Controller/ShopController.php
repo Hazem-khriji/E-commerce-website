c6026e20cd5103c08740703bb28f83d77fd7e768
MohamedAliHG <medalihajamgabsi@gmail.com>
2025-05-12 14:47:25 +0100

    hundle search queries in shop route

--- src/Controller/ShopController.php
+++ src/Controller/ShopController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -11,9 +12,17 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 final class ShopController extends AbstractController
 {
     #[Route('/shop', name: 'app_shop'),IsGranted("ROLE_USER")]
-    public function index(ProductRepository $productRepository): Response
+    public function index(Request $request,ProductRepository $productRepository): Response
     {
-        $products = $productRepository->findAll();
+        $searchTerm = $request->query->get('query');
+
+        if ($searchTerm) {
+            $products = $productRepository->findBySearchQuery($searchTerm);
+        } else {
+            $products = $productRepository->findAll();
+        }
+
+
 
         return $this->render('shop/shop.html.twig', [
             'controller_name' => 'ShopController',
